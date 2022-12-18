86ced827058e4e18443346e4e6b67ce5eb71c4af
durotimicodes <edmondfagbuyi@gmail.com>
2022-12-18 20:05:39 +0000

    db connection with authentication service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "database/sql"
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 const webPort = "85"
+
+var count int64
 
 type Config struct {
 	DB     *sql.DB
@@ -11,4 +20,51 @@ type Config struct {
 
 func main() {
 
+	log.Println("Starting authentication service")
+
+	//TODO connect to DB
+
+	//setup config
+	app := Config{}
+
+	//setup a webserver
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%:s", webPort),
+		Handler: app.routes(),
+	}
+
+	//start the web server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
+
+func OpenDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+//connectToDB maintains connection to the post database 
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := OpenDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready...")
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+	}
 }
